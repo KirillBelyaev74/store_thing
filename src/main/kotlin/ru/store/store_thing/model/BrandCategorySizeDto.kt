44700a0cbad637119d5ac0a375bc206c080bc7d5
KirillBelyaev74 <kirbel74@gmail.com>
2023-04-27 14:56:19 +0500

    add Log

--- src/main/kotlin/ru/store/store_thing/model/BrandCategorySizeDto.kt
+++ src/main/kotlin/ru/store/store_thing/model/BrandCategorySizeDto.kt
@@ -1,6 +1,10 @@
 package ru.store.store_thing.model
 
+import ru.logging.annotation.LogFieldSkip
+import ru.logging.model.ObjectToStringForLog
+
 data class BrandCategorySizeDto(
+    @LogFieldSkip
     val id: Long,
-    val brandCategorySize: String?= null
-)
+    val brandCategorySize: String? = null
+) : ObjectToStringForLog()
--- src/main/kotlin/ru/store/store_thing/model/ThingDto.kt
+++ src/main/kotlin/ru/store/store_thing/model/ThingDto.kt
@@ -1,10 +1,14 @@
 package ru.store.store_thing.model
 
+import ru.logging.annotation.LogFieldSkip
+import ru.logging.model.ObjectToStringForLog
+
 class ThingDto(
-        var id: Long? = null,
-        var brand: String? = null,
-        var category: String? = null,
-        var size: String? = null,
-        var price: Long? = null,
-        var description: String? = null
-)
+    @LogFieldSkip
+    var id: Long? = null,
+    var brand: String? = null,
+    var category: String? = null,
+    var size: String? = null,
+    var price: Long? = null,
+    var description: String? = null
+) : ObjectToStringForLog()
